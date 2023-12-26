1c5ba78657486c9f6434e0fcc33ff549542fe9c8
Andrey Nurullin <andrey.nurullin@gmail.com>
2023-12-26 10:12:12 +0300

    fix: вынес коды ошибок в константы

--- utils/utils.js
+++ utils/utils.js
@@ -1,10 +1,14 @@
+const CODE_INVALID_DATA = 400;
+const CODE_NOT_FOUND = 404;
+const CODE_INTERNAL_ERROR = 500;
+
 module.exports.handleError = (err, res) => {
   switch (err.name) {
     case 'ValidationError':
-      return res.status(400).send({ message: 'Некорректные данные' });
+      return res.status(CODE_INVALID_DATA).send({ message: 'Некорректные данные' });
     case 'CastError':
-      return res.status(404).send({ message: 'Запрашиваемый ресурс не найден' });
+      return res.status(CODE_NOT_FOUND).send({ message: 'Запрашиваемый ресурс не найден' });
     default:
-      return res.status(500).send({ message: 'Ошибка сервера' });
+      return res.status(CODE_INTERNAL_ERROR).send({ message: 'Ошибка сервера' });
   }
 };
