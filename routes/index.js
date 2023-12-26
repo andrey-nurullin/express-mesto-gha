0b64fa9174117533c03527615cb2e9955f7294aa
Andrey Nurullin <andrey.nurullin@gmail.com>
2023-12-26 12:57:10 +0300

    fix: обработка ошибки 404 страница не найдена

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
 const userRouter = require('./users');
 const cardRouter = require('./cards');
+const { NotFoundError, handleError } = require('../utils/utils');
 
 router.use('/users', userRouter);
 router.use('/cards', cardRouter);
+router.use('*', (req, res) => handleError(new NotFoundError(), res));
 
 module.exports = router;
