4a82b1aa1fd18b270568c6361716fd410ac4e365
Andrey Nurullin <andrey.nurullin@gmail.com>
2024-01-31 14:29:41 +0300

    fix: выполнение кода продолжалось и приводило к ошибке

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -6,6 +6,7 @@ module.exports = (req, res, next) => {
   const { authorization } = req.headers;
   if (!authorization || !authorization.startsWith('Bearer ')) {
     next(new AuthError());
+    return;
   }
 
   const token = authorization.replace('Bearer ', '');
